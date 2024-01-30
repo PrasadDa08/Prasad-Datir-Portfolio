44b60106283ac027928da8f368388ab50c48e815
PrasadDa08 <prasaddatir05@gmail.com>
2024-01-31 05:07:42 +0530

    Update - responsive-ProjectPage

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact Page</div>;
+  return (
+    <form
+      action="https://formspree.io/f/mwkgvddz"
+      method="POST"
+      style={{ margin: "auto" }}
+    >
+      <label>
+        Name:
+        <input type="text" name="name" required />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" required />
+      </label>
+      <label>
+        Phone:
+        <input type="number" name="phone" />
+      </label>
+      <label>
+        Message:
+        <textarea name="message" required />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,12 @@ function Projects() {
       <div className="projects">
         <h1>My Projects</h1>
         <h6>
-          Given below are the major projects which I have created, for seeing
-          more projects visit my github.
+          Browse through a selection of major projects I've developed below. For
+          an extensive collection of my work, you can explore my GitHub
+          repository. These projects showcase my skills and expertise in various
+          domains, providing a glimpse into my passion for coding and
+          problem-solving. Feel free to explore and delve into the details of
+          each project to get a comprehensive view of my work.
         </h6>
         <ProjectList />
       </div>
